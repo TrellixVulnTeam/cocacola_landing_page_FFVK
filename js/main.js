cd434c95f4fda08bc36b1c499e6d81bebc4d8dac
saebyeolp <saebyeolpak@gmail.com>
2021-06-24 11:33:45 -0700

    add navigation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 
+const toggleMenu = () => {
+    const menuToggle = document.querySelector('.toggle')
+    const navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('mobile-menu')
+}
