317bfdf1beda547f23ffbeea0c028abfc4c39f99
saebyeolp <saebyeolpak@gmail.com>
2021-06-25 12:01:07 -0700

    add slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,47 @@
 
+/*/******************* Menu Toggle ******************* /*/
+
 const toggleMenu = () => {
     const menuToggle = document.querySelector('.toggle')
     const navigation = document.querySelector('.navigation')
     menuToggle.classList.toggle('active')
     navigation.classList.toggle('mobile-menu')
+}
+
+
+
+/*/******************* Vertical Slide ******************* /*/
+
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 70  }vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+
+    console.log('slideHeight : ', sliderHeight)
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 }
