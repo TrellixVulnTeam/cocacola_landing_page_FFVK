fc8c55e45d59e385c974d5d633e391b2a2636aef
saebyeolp <saebyeolpak@gmail.com>
2021-07-03 10:48:10 -0700

    fix slider

--- js/main.js
+++ js/main.js
@@ -27,20 +27,36 @@ const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
 const slidesLength = slideRight.querySelectorAll('div').length
 
+const main = document.querySelector('.main')
 
 let activeSlideIndex = 0
 let width = window.innerWidth;
 
 
-slideLeft.style.top = width < 599 ? `-${(slidesLength - 1) * 50 }vh` : `-${(slidesLength - 1) * 100 }vh`
+// slide picture position
 
-window.onresize = function() {
-    if (width < 599) {
-        slideLeft.style.top = `-${(slidesLength - 1) * 50 }vh`
+const widths = [0, 600, 980];
+
+const resizeFn = () => {
+    if (window.innerWidth>=widths[0] && window.innerWidth<widths[1]) {
+        slideLeft.style.top = `-${(slidesLength - 1) * 50 }vh` 
+        slideLeft.style.transform = `translateY(0)` 
+        slideRight.style.transform = `translateY(0)` 
+        console.log('tablet')
+    } else if (window.innerWidth>=widths[1] && window.innerWidth<widths[2]) {
+        slideLeft.style.top = `-${(slidesLength - 1) * 100 }vh`
+        slideLeft.style.transform = `translateY(0)` 
+        slideRight.style.transform = `translateY(0)` 
+        console.log('pc')
     } else {
         slideLeft.style.top = `-${(slidesLength - 1) * 100 }vh`
+        slideLeft.style.transform = `translateY(0)` 
+        slideRight.style.transform = `translateY(0)` 
+        console.log('pc')
     }
 }
+window.onresize = resizeFn;
+resizeFn();
 
 
 upButton.addEventListener('click', () => changeSlide('up'))
@@ -62,8 +78,9 @@ const changeSlide = (direction) => {
     
     const sliderHeight = sliderContainer.clientHeight
     let slidePosition = `${(activeSlideIndex * sliderHeight)}`
+    let width = window.innerWidth;
 
-    slideRight.style.transform = width < 599 ? `translateY(-${slidePosition/2}px)`: `translateY(-${slidePosition}px)`
-    slideLeft.style.transform = width < 599 ? `translateY(${(slidePosition)/2}px)` : `translateY(${slidePosition}px)`
+    slideRight.style.transform = width < 600 ? `translateY(-${slidePosition/2}px)`: `translateY(-${slidePosition}px)`
+    slideLeft.style.transform = width < 600 ? `translateY(${(slidePosition)/2}px)` : `translateY(${slidePosition}px)`
 
 }
