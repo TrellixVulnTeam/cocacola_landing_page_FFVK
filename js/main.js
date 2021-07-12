5b9a04e87caa71371a526bf510ffa461573aafc9
saebyeolp <saebyeolpak@gmail.com>
2021-07-12 14:16:31 -0700

    add auto text effect

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 
+/*/******************* Auto Text ******************* /*/
+
+const textEl = document.getElementById('text')
+const text = 'Taste the Feeling'
+let idx = 1
+let speed = 100
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+    idx++
+
+    if(idx > text.length) {
+        idx = idx
+    }
+
+    setTimeout(writeText, speed)
+}
+
+
+
 /*/******************* Menu Toggle ******************* /*/
 
 const menuToggle = document.querySelector('.toggle')
